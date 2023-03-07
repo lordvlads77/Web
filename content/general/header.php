86892db0df31d00cf9da55614444b78bc91998ef
LordVlads <zap363@amerike.edu.mx>
2023-03-06 20:48:08 -0600

    Clase del 060323

---
+++ content/general/header.php
@@ -0,0 +1,47 @@
+<header>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<div id="carouselExample" class="carousel slide">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</header>
--- index.php
+++ index.php
@@ -1,8 +1,20 @@
 <?php
-    print_r($_GET)
-?>
+    if(isset($_GET["file"]) and trim($_GET["file"]) !=""){
+        $section = $_GET["file"];
+    }else{
+        $section = "index";
+    }
+    //echo '<pre>'.print_r($_SERVER,true).'</pre>';
+    $dir = $_SERVER["DOCUMENT_ROOT"]."/practica1/content/";
+    $file = $dir."$section.php";
+    if(file_exists($file) and is_file($file)){
+        include($dir.'general/top.php');
+        include($file);
+        include($dir.'general/header.php');
+        include($dir.'general/bottom.php');
+    }else{
+        echo '404 archivo no encontrado';
+    }
 
-if (file_exists)
-{
-    
-}
+
+?>
