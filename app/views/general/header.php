449bb6f0cc82f22e2f4b72279174bbc93b0a08ce
Eva Luna Álvarez <zap363@amerike.edu.mx>
2023-05-03 04:56:11 -0600

    Modified the css file | Made the Carousel autoplay | Added Indicators

--- app/views/general/header.php
+++ app/views/general/header.php
@@ -5,7 +5,7 @@
 
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">Fancy Logo</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -32,7 +32,13 @@
     </div>
   </div>
 </nav>
-<div id="carouselExample" class="carousel slide">
+<div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
+<div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img src="img/DarthMarr.jpg" class="d-block w-100" alt="...">
